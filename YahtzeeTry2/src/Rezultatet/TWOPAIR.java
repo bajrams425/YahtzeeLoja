055bf893a04a6215e06b59af0db1509dfa1f5d87
bajrams425 <bajrams425@gmail.com>
2020-05-18 20:32:45 +0200

    Add files via upload

--- YahtzeeTry2/src/Rezultatet/TWOPAIR.java
+++ YahtzeeTry2/src/Rezultatet/TWOPAIR.java
@@ -3,20 +3,40 @@ package Rezultatet;
 public class TWOPAIR {
 
     private int shuma = 0;
+    private int count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0, count6 = 0;
     private int count = 0;
-
+    private int[] checkPairs = new int[6];
 
     public TWOPAIR(int[] rezultatiRoll){
 
         for(int i = 0; i<rezultatiRoll.length; i++){
-            for(int j = 1; j<rezultatiRoll.length; j++){
-                if(rezultatiRoll[i]==rezultatiRoll[j]){
-                    count++;
+                switch(rezultatiRoll[i]){
+                    case 1: count1++; break;
+                    case 2: count2++; break;
+                    case 3: count3++; break;
+                    case 4: count4++; break;
+                    case 5: count5++; break;
+                    case 6: count6++; break;
                 }
-                if (count == 2){
-                    shuma = 15;
-                    break;
-                }
+        }
+        for(int k = 0; k<6; k++){
+            switch(k){
+                case 0: checkPairs[k]=count1; break;
+                case 1: checkPairs[k]=count2; break;
+                case 2: checkPairs[k]=count3; break;
+                case 3: checkPairs[k]=count4; break;
+                case 4: checkPairs[k]=count5; break;
+                case 5: checkPairs[k]=count6; break;
+            }
+        }
+
+        for(int j = 0; j<checkPairs.length; j++){
+            if(checkPairs[j]==2){
+                count++;
+            }
+            if(count==2){
+                shuma=15;
+                break;
             }
         }
     }
